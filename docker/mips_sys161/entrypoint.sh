cd5712a4a7083d0a40b3dc345a28b6c9694bba4f
Alex Patel <patelalex02@gmail.com>
2017-08-16 11:56:37 -0400

    drop mips circleci build artifacts, hake output just gets put into main build log

--- docker/mips_sys161/entrypoint.sh
+++ docker/mips_sys161/entrypoint.sh
@@ -4,4 +4,4 @@ set -e
 
 mkdir /usr/local/barrelfish/build
 cd /usr/local/barrelfish/build
-../hake/hake.sh -s .. -a mips > $CIRCLE_ARTIFACTS/hakelog.out 2> $CIRCLE_ARTIFACTS/hakelog.err
+../hake/hake.sh -s .. -a mips
