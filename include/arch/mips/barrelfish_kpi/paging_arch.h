ef2650bbfb0ad00d26110a7d144b781253866e3c
CrystalMei <jingmei_hu@g.harvard.edu>
2017-09-05 15:52:08 -0400

    add paging_arch.h in mips (currently empty)

---
+++ include/arch/mips/barrelfish_kpi/paging_arch.h
@@ -0,0 +1,23 @@
+/**
+ * \file
+ * \brief Define generics for arch specific definitions
+ */
+
+/*
+ * Copyright (c) 2010-2013 ETH Zurich.
+ * All rights reserved.
+ *
+ * This file is distributed under the terms in the attached LICENSE file.
+ * If you do not find this file, copies can be found by writing to:
+ * ETH Zurich D-INFK, Universitaetstrasse 6, CH-8092 Zurich. Attn: Systems Group.
+ */
+
+#ifndef ARCH_MIPS_BARRELFISH_KPI_PAGING_H
+#define ARCH_MIPS_BARRELFISH_KPI_PAGING_H
+
+/**
+ * MISSING something here
+ */
+
+
+#endif // ARCH_MIPS_BARRELFISH_KPI_PAGING_H
